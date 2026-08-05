268927de6f93daef08fa579c736297f5c4d86d18
sravalika <sravalika94@gmail.com>
2026-08-05 17:56:54 +0530

    Time: 548 ms (64.29%) | Memory: 180 MB (21.43%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def remainingMethods(self, n, k, invocations):
+        graph = [[] for _ in range(n)]
+
+        for u, v in invocations:
+            graph[u].append(v)
+
+        suspicious = [False] * n
+
+        def dfs(u):
+            suspicious[u] = True
+            for v in graph[u]:
+                if not suspicious[v]:
+                    dfs(v)
+
+        dfs(k)
+
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
+
+        return [i for i in range(n) if not suspicious[i]]
+        
