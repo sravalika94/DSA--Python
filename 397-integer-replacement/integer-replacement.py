fa498db92ee893c0374c3728fef5c564fcca71c1
sravalika <sravalika94@gmail.com>
2026-08-07 18:34:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (52.80%) - LeetSync

---
+++ 397-integer-replacement/integer-replacement.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def integerReplacement(self, n):
+        steps = 0
+
+        while n != 1:
+            if n % 2 == 0:
+                n //= 2
+            elif n == 3 or n % 4 == 1:
+                n -= 1
+            else:
+                n += 1
+            steps += 1
+
+        return steps
+        
