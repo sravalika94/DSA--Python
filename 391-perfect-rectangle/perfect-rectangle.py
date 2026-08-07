269af5ccbed923fff76d6eaac922a5fbeaf7ba8a
sravalika <sravalika94@gmail.com>
2026-08-07 18:25:23 +0530

    Time: 55 ms (21.89%) | Memory: 16.6 MB (54.44%) - LeetSync

---
+++ 391-perfect-rectangle/perfect-rectangle.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isRectangleCover(self, rectangles):
+        area = 0
+        corners = set()
+
+        min_x = min_y = float('inf')
+        max_x = max_y = float('-inf')
+
+        for x1, y1, x2, y2 in rectangles:
+            area += (x2 - x1) * (y2 - y1)
+
+            min_x = min(min_x, x1)
+            min_y = min(min_y, y1)
+            max_x = max(max_x, x2)
+            max_y = max(max_y, y2)
+
+            for p in [(x1, y1), (x1, y2), (x2, y1), (x2, y2)]:
+                if p in corners:
+                    corners.remove(p)
+                else:
+                    corners.add(p)
+
+        expected = {
+            (min_x, min_y),
+            (min_x, max_y),
+            (max_x, min_y),
+            (max_x, max_y)
+        }
+
+        return area == (max_x - min_x) * (max_y - min_y) and corners == expected
+        
