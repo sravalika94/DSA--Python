4d608420b845f2921573829d4b42957af55971a2
sravalika <sravalika94@gmail.com>
2026-08-07 18:30:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (18.23%) - LeetSync

---
+++ 394-decode-string/decode-string.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def decodeString(self, s):
+        stack = []
+        curr = ""
+        num = 0
+
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == '[':
+                stack.append((curr, num))
+                curr = ""
+                num = 0
+            elif ch == ']':
+                prev, k = stack.pop()
+                curr = prev + curr * k
+            else:
+                curr += ch
+
+        return curr
+        
