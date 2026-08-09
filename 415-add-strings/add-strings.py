6a303ad21e5f3750b3087a4a75c642dc28e89f63
sravalika <sravalika94@gmail.com>
2026-08-09 16:21:11 +0530

    Time: 7 ms (41.13%) | Memory: 12.6 MB (19.46%) - LeetSync

---
+++ 415-add-strings/add-strings.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        i = len(num1) - 1
+        j = len(num2) - 1
+        carry = 0
+        ans = ""
+
+        while i >= 0 or j >= 0 or carry:
+            x = ord(num1[i]) - ord('0') if i >= 0 else 0
+            y = ord(num2[j]) - ord('0') if j >= 0 else 0
+
+            s = x + y + carry
+            ans = chr(s % 10 + ord('0')) + ans
+            carry = s // 10
+
+            i -= 1
+            j -= 1
+
+        return ans
+        
