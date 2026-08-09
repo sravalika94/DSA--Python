fe46b3846cef0f41ba54c81e4d5263f086e0f72e
sravalika <sravalika94@gmail.com>
2026-08-09 16:09:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (89.78%) - LeetSync

---
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reconstructQueue(self, people):
+        people.sort(key=lambda x: (-x[0], x[1]))
+
+        ans = []
+
+        for p in people:
+            ans.insert(p[1], p)
+
+        return ans
+        
