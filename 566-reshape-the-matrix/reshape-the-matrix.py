db7a433c6065b0705e68cfb9cb333b129ce2e11c
sravalika <sravalika94@gmail.com>
2026-08-19 15:36:36 +0530

    Time: 2 ms (45.99%) | Memory: 13 MB (43.47%) - LeetSync

---
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def matrixReshape(self, mat, r, c):
+        m, n = len(mat), len(mat[0])
+
+        if m * n != r * c:
+            return mat
+
+        ans = [[0] * c for _ in range(r)]
+
+        for i in range(m * n):
+            ans[i // c][i % c] = mat[i // n][i % n]
+
+        return ans
+        
