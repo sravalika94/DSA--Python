0b430696346e7aa0b88c13b32cd5cc05b3fd7feb
sravalika <sravalika94@gmail.com>
2026-08-23 15:41:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (55.21%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        left = 0
+        right = len(arr) - k
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if x - arr[mid] > arr[mid + k] - x:
+                left = mid + 1
+            else:
+                right = mid
+
+        return arr[left:left + k]
+        
