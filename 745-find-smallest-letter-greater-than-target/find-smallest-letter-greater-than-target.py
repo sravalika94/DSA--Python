a4096c83c9286bff6a409ca85d7f69a21a56b5bc
sravalika <sravalika94@gmail.com>
2026-09-07 22:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (77.69%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        for ch in letters:
+            if ch > target:
+                return ch
+        return letters[0]
+        
